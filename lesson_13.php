8c8595805449e8242af02da639a1aaf4340929b3
pavel <ppyakov@gmail.com>
2017-10-30 10:25:20 +0300

    final

--- lesson_13.php
+++ lesson_13.php
@@ -3,13 +3,16 @@ $pdo = new PDO('mysql:host=localhost;dbname=global;charset=UTF8', 'root', 'qwert
 $sql = 'SELECT name, author, year, isbn, genre FROM books';
 
 function getTable($row){
-	echo '<tr>';
-	echo '<td>'.$row['name'].'</td>';
-	echo '<td id="center">'.$row['author'].'</td>';
-	echo '<td id="center">'.$row['year'].'</td>';
-	echo '<td>'.$row['genre'].'</td>';
-	echo '<td id="center">'.$row['isbn'].'</td>';
-	echo '</tr>';
+	return '<tr>'.'<td>'.$row['name'].'</td>'.'<td id="center">'.$row['author'].'</td>'.'<td id="center">'.$row['year'].'</td>'.'<td>'.$row['genre'].'</td>'.'<td id="center">'.$row['isbn'].'</td>'.'</tr>';
+}
+
+function getValue($vl){
+	if(empty($_GET) or empty($_GET[strval($vl)])){
+		return '';
+	}
+	else{
+		return strip_tags($_GET[strval($vl)]);
+	}
 }
 ?>
 <html> 
@@ -37,30 +40,9 @@ function getTable($row){
 <body>
 <h2>Список книг</h2>
 <form method="GET">
-    <input type="text" name="isbn" placeholder="ISBN" value="<?php
-		if(empty($_GET['isbn'])){
-			echo '';
-		}
-		else{
-			echo trim($_GET['isbn']);
-		}
-	?>"/>
-    <input type="text" name="name" placeholder="Название книги" value="<?php
-		if(empty($_GET['name'])){
-			echo '';
-		}
-		else{
-			echo trim($_GET['name']);
-		}
-	?>"/>
-    <input type="text" name="author" placeholder="Автор книги" value="<?php
-		if(empty($_GET['author'])){
-			echo '';
-		}
-		else{
-			echo trim($_GET['author']);
-		}
-	?>"/>
+    <input type="text" name="isbn" placeholder="ISBN" value="<?php echo getValue('isbn');?>"/>
+    <input type="text" name="name" placeholder="Название книги" value="<?php echo getValue('name');?>"/>
+    <input type="text" name="author" placeholder="Автор книги" value="<?php echo getValue('author'); ?>"/>
     <input type="submit" value="Поиск" />
 </form>
 </br>
@@ -75,27 +57,27 @@ function getTable($row){
 <?php
 if(empty($_GET)){
 	foreach ($pdo->query($sql) as $row) {
-		getTable($row);
+		echo getTable($row);
 	}
 }
 else if(!empty($_GET['isbn'])){
 	foreach ($pdo->query('SELECT name, author, year, isbn, genre FROM books WHERE isbn='.'"'.$_GET['isbn'].'"') as $row) {
-		getTable($row);
+		echo getTable($row);
 	}
 }
 else if(!empty($_GET['name'])){
 	foreach ($pdo->query('SELECT name, author, year, isbn, genre FROM books WHERE name='.'"'.$_GET['name'].'"') as $row) {
-		getTable($row);
+		echo getTable($row);
 	}
 }
 else if(!empty($_GET['author'])){
 	foreach ($pdo->query('SELECT name, author, year, isbn, genre FROM books WHERE author='.'"'.$_GET['author'].'"') as $row) {
-		getTable($row);
+		echo getTable($row);
 	}
 }
 else{
 	foreach ($pdo->query($sql) as $row) {
-		getTable($row);
+		echo getTable($row);
 	}
 }
 ?> 
